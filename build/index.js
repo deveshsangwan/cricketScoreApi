e7dfe69ea98356166cff54581d4250c867cffcad
deveshsangwan <dev.sangwan2001@gmail.com>
2023-02-25 22:36:52 +0530

    initial commit

---
+++ build/index.js
@@ -0,0 +1,9 @@
+global.__basedir = process.cwd() + '/';
+const port = process.env.NODE_PORT || 3000;
+const app           = require(__basedir + 'app/app.js');
+//const config = require(__basePath + 'app/core/configuration');
+//const port = process.env.NODE_PORT || config.get('server:index:port');
+
+app.listen(port, function () {
+    console.log(`Running on port: ${port}`);
+});
