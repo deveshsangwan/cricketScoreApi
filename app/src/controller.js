f06faeafad09f661b5eb181d50ba4680b15a99c6
deveshsangwan <dev.sangwan2001@gmail.com>
2023-02-26 18:04:00 +0530

    added getMatches api

--- app/src/controller.js
+++ app/src/controller.js
@@ -1,11 +1,16 @@
-const liveEvents = require('./LiveMatches/LiveMatches');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const LiveMatches_1 = require("./LiveMatches/LiveMatches");
 const live = function (req, res) {
     console.log('live');
+    const liveMatchesObj = new LiveMatches_1.LiveMatches();
+    const liveMatchesResponse = liveMatchesObj.getMatches();
+    console.log('liveMatchesResponse', liveMatchesResponse);
     // console.log(req);
     return res.status(200).send({
         status: true,
         message: 'Live Matches',
-        response: "conda"
+        response: liveMatchesResponse
     });
 };
 exports.live = live;
