6fd18260d109b4d48d38ccb6e489c5e4fb947c68
deveshsangwan <dev.sangwan2001@gmail.com>
2023-03-18 20:17:17 +0530

    matchStats api integrated

--- app/ts_src/MatchStats/MatchStats.ts
+++ app/ts_src/MatchStats/MatchStats.ts
@@ -20,7 +20,7 @@ export class MatchStats {
                     this.matchId = key;
                     const scrapedData = await this.scrapeData(liveMatchesResponse[key].matchUrl);
                     data.push(scrapedData);
-                    break;
+                    //break;
                 }
             } else {
                 const scrapedData = await this.scrapeData(this.matchId);
@@ -36,8 +36,8 @@ export class MatchStats {
 
             const options = {
                 //url: 'https://www.cricbuzz.com' + this.matchId,
-                //url: 'https://www.cricbuzz.com' + url,
-                url: 'https://www.cricbuzz.com/live-cricket-scores/50160/wa-vs-vic-30th-match-sheffield-shield-2022-23',
+                url: 'https://www.cricbuzz.com' + url,
+                //url: 'https://www.cricbuzz.com/live-cricket-scores/50160/wa-vs-vic-30th-match-sheffield-shield-2022-23',
                 headers: {
                     'User-Agent': 'request'
                 }
@@ -46,72 +46,10 @@ export class MatchStats {
             let matchStats = {}
             let tournamentName = await this.getTournamentName(options);
 
-            // request(options, (error, response, html) => {
-            //     if (!error && response.statusCode == 200) {
-            //         const $ = cheerio.load(html);
-
-            //         $('.cb-mini-scr-col').each((i, el) => {
-            //             //console.log('el', el);
-            //             const currentTeamData = $(el).find('.cb-text-gray.cb-font-16').text().trim();
-            //             const otherTeamData = $(el).find('.cb-font-20.text-bold').text().trim();
-            //             const currentSummary = $(el).find('.cb-text-inprogress').text().trim();
-            //             let batsmanData = $(el).find('.cb-min-inf.cb-col-100').find('.cb-col.cb-col-50').text();
-
-            //             $(el).find('.cb-min-inf.cb-col-100').each((i, el) => {
-            //                 console.log('html====================', $(el).html());
-            //                 // console.log('eeeeeeeeeeeeeeeeeeeeee', $(el).find('.cb-text-link').text());
-            //                 // console.log('ffffffffffffffffffffff', $(el).find('.cb-text-link').next().html());
-            //                 // // if () {
-            //                 //     console.log('el', $(el).text());
-            //                 // // }
-            //             });
-            //             console.log('batsmanData', batsmanData);
-            //             //console.log('currentTeamData', currentTeamData);
-            //             //console.log('otherTeamData', otherTeamData);
-            //             /*
-            //             1. replace the brackets
-            //             2. break the string by spaces and slashes 
-            //             3. remove the empty string
-            //             */
-            //             const currentTeamDataArray = currentTeamData.replace(/[\(\)]/g, '').split(/[/\s]/).filter(Boolean);
-            //             const otherTeamDataArray = otherTeamData.replace(/[\(\)]/g, '').split(/[/\s]/).filter(Boolean);
-
-            //             matchStats = {
-            //                 matchId: this.matchId,
-            //                 tournamentName: tournamentName,
-            //                 team1: !currentTeamDataArray[0] ? {} : {
-            //                     isBatting: true,
-            //                     name: currentTeamDataArray[0],
-            //                     score: currentTeamDataArray[1],
-            //                     overs: currentTeamDataArray.length > 3 ? currentTeamDataArray[3] : currentTeamDataArray[2],
-            //                     wickets: currentTeamDataArray.length > 3 ? currentTeamDataArray[2] : 10,
-            //                 },
-            //                 team2: !otherTeamDataArray[0] ? {} : {
-            //                     name: otherTeamDataArray[0],
-            //                     score: otherTeamDataArray[1],
-            //                     overs: otherTeamDataArray.length > 3 ? otherTeamDataArray[3] : otherTeamDataArray[2],
-            //                     wickets: otherTeamDataArray.length > 3 ? otherTeamDataArray[2] : 10,
-            //                 },
-            //                 onBatting: {
-            //                     player1: {
-
-            //                     },
-            //                     player2: {
-            //                     }
-            //                 },
-            //                 summary: currentSummary
-            //             }
-
-            //             //console.log('matchStats', matchStats);
-            //             return resolve(matchStats);
-            //         });
-            //         return resolve(matchStats);
-            //     }
-            // });
-
             // replace www with m in options.url
             options.url = options.url.replace('www', 'm');
             let response = await this.getMatchStatsByMatchId(options);
+            response['tournamentName'] = tournamentName;
 
             return resolve(response);
         });
@@ -135,21 +73,55 @@ export class MatchStats {
     }
 
     public async getMatchStatsByMatchId(options): Promise<{}> {
-        return new Promise(async (resolve, reject) => {
+        return new Promise((resolve, reject) => {
             let matchData = {};
 
             request(options, (error, response, html) => {
                 if (!error && response.statusCode == 200) {
                     const $ = cheerio.load(html);
 
-                    let currentTeamData = $('span.ui-bat-team-scores').text();
-                    let currentBatsman = $('span.bat-bowl-miniscore').eq(0).text();
-                    console.log('currentBatsman', currentBatsman);
-                    console.log('currentTeamData', currentTeamData);
+                    // split the string by spaces, -, / and brackets
+                    const currentTeamDataArray = $('span.ui-bat-team-scores').text().trim().split(/[/\s/\-/\(/\)]/).filter(Boolean);
+                    const otherTeamDataArray = $('span.ui-bowl-team-scores').text().trim().split(/[/\s/\-/\(/\)]/).filter(Boolean);
+                    const currentBatsman = $('span.bat-bowl-miniscore').eq(0).text().replace('*', '');
+                    const [ currentBatsmanRuns, currentBatsmanBalls ] = $('td[class="cbz-grid-table-fix "]').eq(6).text().split('(').map((item) => item.replace(/[\(\)]/g, ''));
+                    const otherBatsman = $('span.bat-bowl-miniscore').eq(1).text();
+                    const [ otherBatsmanRuns, otherBatsmanBalls ] = $('td[class="cbz-grid-table-fix "]').eq(11).text().split('(').map((item) => item.replace(/[\(\)]/g, ''));
+                    
+                    matchData = {
+                        matchId: this.matchId,
+                        team1: !currentTeamDataArray[0] ? {} : {
+                            isBatting: true,
+                            name: currentTeamDataArray[0],
+                            score: currentTeamDataArray[1],
+                            overs: currentTeamDataArray.length > 3 ? currentTeamDataArray[3] : currentTeamDataArray[2],
+                            wickets: currentTeamDataArray.length > 3 ? currentTeamDataArray[2] : 10,
+                        },
+                        team2: !otherTeamDataArray[0] ? {} : {
+                            name: otherTeamDataArray[0],
+                            score: otherTeamDataArray[1],
+                            overs: otherTeamDataArray.length > 3 ? otherTeamDataArray[3] : otherTeamDataArray[2],
+                            wickets: otherTeamDataArray.length > 3 ? otherTeamDataArray[2] : 10,
+                        },
+                        onBatting: {
+                            player1: {
+                                name: currentBatsman,
+                                onStrike: true,
+                                runs: currentBatsmanRuns,
+                                balls: currentBatsmanBalls
+                            },
+                            player2: {
+                                name: otherBatsman,
+                                runs: otherBatsmanRuns,
+                                balls: otherBatsmanBalls
+                            }
+                        },
+                        summary: $("div.cbz-ui-status").text().trim()
+                    };
+
+                    return resolve(matchData);
                 }
             });
-
-            return resolve(matchData);
         });
     }
 }
