807595082e3f719c94efa0a167c3bb1c081b2c9b
deveshsangwan <dev.sangwan2001@gmail.com>
2024-01-22 23:50:56 +0530

    firebase setup

--- functions/index.js
+++ functions/index.js
@@ -7,8 +7,12 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+global.__basedir = process.cwd() + '/';
+const functions = require('firebase-functions');
+const app = require(__basedir + 'app/app.js');
+
+// Export the app as a function that Firebase can use
+exports.app = functions.https.onRequest(app);
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
