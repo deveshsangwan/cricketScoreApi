51e9676671841aed0df9c2f0b13945dc07fdcbb5
deveshsangwan <dev.sangwan2001@gmail.com>
2024-01-26 22:34:43 +0530

    refactored mongo baseModel | code cleaning 🧹

--- app/core/baseModel.js
+++ app/core/baseModel.js
@@ -54,42 +54,32 @@ const matchStats = new Mongoose.Schema({
     },
 });
 
-// find all matches
-const findAll = (stats = false) => {
-    if (stats) {
-        return new Promise(async (resolve, reject) => {
-            let response = await Mongoose.model('matchStats', matchStats).find({});
-            resolve(response);
-        }).catch(err => {
-            writeLogError(["findAll MATCH_STATS error: ", err]);
-        });
-    }
+const LiveMatches = Mongoose.model('liveMatches', liveMatches);
+const MatchStats = Mongoose.model('matchStats', matchStats);
 
-    return new Promise(async (resolve, reject) => {
-        let response = await Mongoose.model('liveMatches', liveMatches).find({});
-        resolve(response);
-    }).catch(err => {
-        writeLogError(["findAll LIVE_MATCHES error: ", err]);
-    });
-}
+// find all matches
+const findAll = async (modelName) => {
+    try {
+        const response = await Mongoose.model(modelName).find({});
+        return response;
+    } catch (err) {
+        const collectionName = modelName === 'matchStats' ? 'MATCH_STATS' : 'LIVE_MATCHES';
+        writeLogError([`findAll ${collectionName} error: `, err]);
+        throw err;
+    }
+};
 
 // find match by id
-const findById = (matchId, stats = false) => {
-    if (stats) {
-        return new Promise(async (resolve, reject) => {
-            let response = await Mongoose.model('matchStats', matchStats).find({ _id: matchId });
-            resolve(response);
-        }).catch(err => {
-            writeLogError(["findById MATCH_STATS error: ", err]);
-        });
+const findById = async (matchId, modelName) => {
+    try {
+        const response = await Mongoose.model(modelName).find({ _id: matchId });
+        return response;
+    } catch (err) {
+        const collectionName = modelName === 'matchStats' ? 'MATCH_STATS' : 'LIVE_MATCHES';
+        writeLogError([`findById ${collectionName} error: `, err]);
+        throw err;
     }
-    return new Promise(async (resolve, reject) => {
-        let response = await Mongoose.model('liveMatches', liveMatches).find({ _id: matchId });
-        resolve(response);
-    }).catch(err => {
-        writeLogError(["findById LIVE_MATCHES error: ", err]);
-    });
-}
+};
 
 // find id by match url
 const findIdByMatchUrl = (matchUrl) => {
@@ -101,41 +91,29 @@ const findIdByMatchUrl = (matchUrl) => {
 }
 
 // insert one match
-const insert = (data, stats = false) => {
-    if (stats) {
-        return new Promise(async (resolve, reject) => {
-            let response = await Mongoose.model('matchStats', matchStats).create(data);
-            resolve(response);
-        }).catch(err => {
-            writeLogError(["insert MATCH_STATS error: ", err]);
-        });
+const insert = async (data, modelName) => {
+    try {
+        const response = await Mongoose.model(modelName).create(data);
+        return response;
+    } catch (err) {
+        const collectionName = modelName === 'matchStats' ? 'MATCH_STATS' : 'LIVE_MATCHES';
+        writeLogError([`insert ${collectionName} error: `, err]);
+        throw err;
     }
-    return new Promise(async (resolve, reject) => {
-        let response = await Mongoose.model('liveMatches', liveMatches).create(data);
-        resolve(response);
-    }).catch(err => {
-        writeLogError(["insert LIVE_MATCHES error: ", err]);
-    });
-}
+};
 
 
 // insert multiple matches
-const insertMany = (matches, stats = false) => {
-    if (stats) {
-        return new Promise(async (resolve, reject) => {
-            let response = await Mongoose.model('matchStats', matchStats).insertMany(matches);
-            resolve(response);
-        }).catch(err => {
-            writeLogError(["insertMany MATCH_STATS error: ", err]);
-        });
+const insertMany = async (matches, modelName) => {
+    try {
+        const response = await Mongoose.model(modelName).insertMany(matches);
+        return response;
+    } catch (err) {
+        const collectionName = modelName === 'matchStats' ? 'MATCH_STATS' : 'LIVE_MATCHES';
+        writeLogError([`insertMany ${collectionName} error: `, err]);
+        throw err;
     }
-    return new Promise(async (resolve, reject) => {
-        let response = await Mongoose.model('liveMatches', liveMatches).insertMany(matches);
-        resolve(response);
-    }).catch(err => {
-        writeLogError(["insertMany LIVE_MATCHES error: ", err]);
-    });
-}
+};
 
 
 module.exports = {
--- app/src/MatchStats.js
+++ app/src/MatchStats.js
@@ -13,18 +13,21 @@ exports.MatchStats = void 0;
 const axios = require('axios');
 const cheerio = require('cheerio');
 const LiveMatches_1 = require("./LiveMatches");
+const Utils_1 = require("./Utils");
 const mongo = require('../core/baseModel');
 const _ = require('underscore');
 const logger_1 = require("../core/logger");
 class MatchStats {
     constructor(matchId = "0") {
         this.matchId = matchId;
+        this.tableName = 'matchStats';
+        this.liveMatchesObj = new LiveMatches_1.LiveMatches();
+        this.utilsObj = new Utils_1.Utils();
     }
     getMatchStats() {
         return __awaiter(this, void 0, void 0, function* () {
             try {
-                const liveMatchesObj = new LiveMatches_1.LiveMatches();
-                const liveMatchesResponse = yield liveMatchesObj.getMatches(this.matchId);
+                const liveMatchesResponse = yield this.liveMatchesObj.getMatches(this.matchId);
                 if (this.matchId === "0") {
                     return yield this.getStatsForAllMatches(liveMatchesResponse);
                 }
@@ -34,8 +37,8 @@ class MatchStats {
                 return Promise.resolve('Invalid request');
             }
             catch (error) {
-                (0, logger_1.writeLogError)(['matchStats | getMatchStats |', error]);
-                return Promise.resolve("Something went wrong");
+                (0, logger_1.writeLogError)(['matchStats | getMatchStats | error', error]);
+                return Promise.reject("Something went wrong");
             }
         });
     }
@@ -48,13 +51,13 @@ class MatchStats {
                 _.extend(scrapedData, { matchName: liveMatchesResponse[key].matchName });
                 data.push(scrapedData);
                 // save data to db if not already exists
-                const mongoData = yield mongo.findById(this.matchId, true);
+                const mongoData = yield mongo.findById(this.matchId, this.tableName);
                 if (!mongoData.length) {
                     let dataToInsert = JSON.parse(JSON.stringify(scrapedData));
                     // repalace key matchId with _id
                     dataToInsert['_id'] = dataToInsert['matchId'];
                     delete dataToInsert['matchId'];
-                    yield mongo.insert(dataToInsert, true);
+                    yield mongo.insert(dataToInsert, this.tableName);
                 }
             }
             if (!data.length) {
@@ -65,7 +68,7 @@ class MatchStats {
     }
     getStatsForSingleMatch(liveMatchesResponse) {
         return __awaiter(this, void 0, void 0, function* () {
-            let mongoData = yield mongo.findById(this.matchId, true);
+            let mongoData = yield mongo.findById(this.matchId, this.tableName);
             if (mongoData.length) {
                 let returnObj = JSON.parse(JSON.stringify(mongoData[0]));
                 // repalce key _id with matchId
@@ -83,7 +86,7 @@ class MatchStats {
                 let dataToInsert = JSON.parse(JSON.stringify(scrapedData));
                 dataToInsert['_id'] = dataToInsert['matchId'];
                 delete dataToInsert['matchId'];
-                yield mongo.insert(dataToInsert, true);
+                yield mongo.insert(dataToInsert, this.tableName);
                 return Promise.resolve(scrapedData);
             }
             return Promise.resolve('Match Id is invalid');
