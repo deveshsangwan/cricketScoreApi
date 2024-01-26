efabf67e4901c2d87bee1e6ea1d6d5acaf1eb8d6
deveshsangwan <dev.sangwan2001@gmail.com>
2024-01-26 23:15:02 +0530

    LiveMatches refactoring 😁

--- app/src/LiveMatches.js
+++ app/src/LiveMatches.js
@@ -10,8 +10,8 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.LiveMatches = void 0;
+const Utils_1 = require("./Utils");
 const logger_1 = require("../core/logger");
-const axios = require('axios');
 const cheerio = require('cheerio');
 const randomstring = require("randomstring");
 const mongo = require('../core/baseModel');
@@ -19,80 +19,81 @@ const _ = require('underscore');
 class LiveMatches {
     constructor() {
         this.tableName = 'liveMatches';
+        this.utilsObj = new Utils_1.Utils();
     }
     getMatches(matchId = "0") {
         return __awaiter(this, void 0, void 0, function* () {
-            try {
-                if (matchId != "0") {
+            if (matchId !== "0") {
+                try {
                     const mongoData = yield mongo.findById(matchId, this.tableName);
                     if (mongoData.length) {
                         mongoData[0]['matchId'] = mongoData[0]['_id'];
                         delete mongoData[0]['_id'];
-                        return Promise.resolve(mongoData[0]);
+                        return mongoData[0];
+                    }
+                    else {
+                        throw new Error(`No match found with id: ${matchId}`);
                     }
                 }
+                catch (error) {
+                    (0, logger_1.writeLogError)(['LiveMatches | getMatches | error', error]);
+                    throw error;
+                }
+            }
+            try {
                 const mongoData = yield mongo.findAll(this.tableName);
                 const scrapedData = yield this.scrapeData(mongoData);
-                return Promise.resolve(scrapedData);
+                return scrapedData;
             }
             catch (error) {
                 (0, logger_1.writeLogError)(['LiveMatches | getMatches | error', error]);
-                return Promise.reject(error);
+                throw error;
             }
         });
     }
     scrapeData(mongoData) {
         return __awaiter(this, void 0, void 0, function* () {
-            const options = {
-                url: 'https://www.cricbuzz.com/cricket-match/live-scores',
-                headers: {
-                    'User-Agent': 'request'
-                }
-            };
-            const matchesData = {}, matchesData1 = {};
             try {
-                const response = yield axios(options);
-                if (response.status === 200) {
-                    const $ = cheerio.load(response.data);
-                    $('.cb-col-100 .cb-col .cb-schdl').each((i, el) => {
-                        const matchUrl = $(el).find('.cb-lv-scr-mtch-hdr a').attr('href');
-                        const matchName = $(el).find('.cb-billing-plans-text a').attr('title');
-                        // if already exists in db, then add it to matchesData
-                        if (mongoData.length && mongoData.find((item) => item.matchUrl === matchUrl)) {
-                            const matchId = mongoData.find((item) => item.matchUrl === matchUrl)._id;
-                            matchesData1[matchId] = {
-                                matchUrl,
-                                matchName
-                            };
-                        }
-                        else if (matchUrl && matchName) {
-                            const matchId = randomstring.generate({
-                                length: 16,
-                                charset: 'alphanumeric'
-                            });
-                            matchesData[matchId] = {
-                                matchUrl,
-                                matchName
-                            };
-                        }
-                    });
-                    // insert new matches into db
-                    let dataToInsert = [];
-                    for (let key in matchesData) {
-                        dataToInsert.push({
-                            _id: key,
-                            matchUrl: matchesData[key].matchUrl,
-                            matchName: matchesData[key].matchName
-                        });
-                    }
-                    yield mongo.insertMany(dataToInsert, this.tableName);
-                }
-                return Promise.resolve(_.extend(matchesData1, matchesData));
+                let url = 'https://www.cricbuzz.com/cricket-match/live-scores';
+                const response = yield this.utilsObj.fetchData(url);
+                let matchesData = this.processData(response, mongoData);
+                // matches data is an array of 2 objects insert the second into db as first is already present and return both after combining
+                yield this.insertData(matchesData[1]);
+                matchesData = _.extend(matchesData[0], matchesData[1]);
+                return matchesData;
             }
             catch (error) {
                 (0, logger_1.writeLogError)(['LiveMatches | scrapeData | error', error]);
-                return Promise.reject(error);
+                throw error;
             }
+        });
+    }
+    processData($, mongoData) {
+        const matchesData = {}, matchesData1 = {};
+        $('.cb-col-100 .cb-col .cb-schdl').each((i, el) => {
+            const matchUrl = $(el).find('.cb-lv-scr-mtch-hdr a').attr('href');
+            const matchName = $(el).find('.cb-billing-plans-text a').attr('title');
+            if (matchUrl && matchName) {
+                const existingMatch = mongoData.find((item) => item.matchUrl === matchUrl);
+                if (existingMatch) {
+                    matchesData1[existingMatch._id] = { matchUrl, matchName };
+                }
+                else {
+                    const matchId = randomstring.generate({ length: 16, charset: 'alphanumeric' });
+                    matchesData[matchId] = { matchUrl, matchName };
+                }
+            }
+        });
+        return [matchesData1, matchesData];
+    }
+    insertData(matchesData) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const dataToInsert = Object.entries(matchesData).map(([key, value]) => ({
+                _id: key,
+                matchUrl: value.matchUrl,
+                matchName: value.matchName
+            }));
+            yield mongo.insertMany(dataToInsert, this.tableName);
         });
     }
 }
