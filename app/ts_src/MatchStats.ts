7c945b94c962218d38ff9ca8da57d308ba5d5be9
deveshsangwan <dev.sangwan2001@gmail.com>
2024-01-27 00:42:34 +0530

    MatchStats perforamance improvement 🚀

--- app/ts_src/MatchStats.ts
+++ app/ts_src/MatchStats.ts
@@ -1,5 +1,3 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
 import { LiveMatches } from './LiveMatches';
 import { Utils } from './Utils';
 const mongo = require('../core/baseModel');
@@ -29,188 +27,161 @@ export class MatchStats {
             const liveMatchesResponse = await this.liveMatchesObj.getMatches(this.matchId);
 
             if (this.matchId === "0") {
-                return await this.getStatsForAllMatches(liveMatchesResponse);
-            } else if (!!this.matchId) {
-                return await this.getStatsForSingleMatch(liveMatchesResponse);
+                return this.getStatsForAllMatches(liveMatchesResponse);
+            } else if (this.matchId) {
+                return this.getStatsForSingleMatch(liveMatchesResponse);
             }
 
-            return Promise.resolve('Invalid request');
+            throw new Error('Invalid request');
         } catch (error) {
             writeLogError(['matchStats | getMatchStats | error', error]);
-            return Promise.reject("Something went wrong");
+            throw new Error("Something went wrong");
         }
     }
 
-    private async getStatsForAllMatches(liveMatchesResponse: LiveMatchesResponse): Promise<{}> {
-        let data = [];
-        for (let key in liveMatchesResponse) {
-            this.matchId = key;
-            const scrapedData = await this.scrapeData(liveMatchesResponse[key].matchUrl);
-            _.extend(scrapedData, { matchName: liveMatchesResponse[key].matchName });
-            data.push(scrapedData);
+    private async getStatsForAllMatches(liveMatchesResponse: LiveMatchesResponse): Promise<{} | 'No matches found'> {
+        const dataPromises = Object.entries(liveMatchesResponse).map(async ([matchId, match]) => {
+            const scrapedData = await this.scrapeData(match.matchUrl);
+            _.extend(scrapedData, { matchName: match.matchName });
 
             // save data to db if not already exists
-            const mongoData = await mongo.findById(this.matchId, this.tableName);
+            const mongoData = await mongo.findById(matchId, this.tableName);
             if (!mongoData.length) {
-                let dataToInsert = JSON.parse(JSON.stringify(scrapedData));
-                // repalace key matchId with _id
-                dataToInsert['_id'] = dataToInsert['matchId'];
-                delete dataToInsert['matchId'];
-                await mongo.insert(dataToInsert, this.tableName);
+                await this.utilsObj.insertDataToMatchStatsTable(scrapedData, matchId);
             }
-        }
+
+            return {...scrapedData, matchId: matchId};
+        });
+
+        const data = await Promise.all(dataPromises);
 
         if (!data.length) {
-            return Promise.resolve('No matches found');
+            return 'No matches found';
         }
-        return Promise.resolve(data);
+
+        return data;
     }
 
-    private async getStatsForSingleMatch(liveMatchesResponse: LiveMatchesResponse): Promise<{}> {
+    private async getStatsForSingleMatch(liveMatchesResponse: LiveMatchesResponse): Promise<{} | 'Match Id is invalid'> {
         let mongoData = await mongo.findById(this.matchId, this.tableName);
         if (mongoData.length) {
-            let returnObj = JSON.parse(JSON.stringify(mongoData[0]));
-            // repalce key _id with matchId
+            // create a deep copy of the object and delete unwanted properties mongoData[0]
+            const returnObj = JSON.parse(JSON.stringify(mongoData[0]));
             returnObj['matchId'] = returnObj['_id'];
-
             delete returnObj['_id'];
             delete returnObj['__v'];
             delete returnObj['createdAt'];
 
-            return Promise.resolve(returnObj);
+            return returnObj;
         } else if (_.has(liveMatchesResponse, 'matchId')) {
             const url = liveMatchesResponse.matchUrl;
             const scrapedData = await this.scrapeData(url);
             _.extend(scrapedData, { matchName: liveMatchesResponse.matchName });
-            // insert data to db
-            let dataToInsert = JSON.parse(JSON.stringify(scrapedData));
-            dataToInsert['_id'] = dataToInsert['matchId'];
-            delete dataToInsert['matchId'];
-            await mongo.insert(dataToInsert, this.tableName);
+            await this.utilsObj.insertDataToMatchStatsTable(scrapedData);
 
-            return Promise.resolve(scrapedData);
+            return scrapedData;
         }
 
-        return Promise.resolve('Match Id is invalid');
+        return 'Match Id is invalid';
     }
 
     private async scrapeData(url): Promise<{}> {
         try {
             if (!this.matchId) return Promise.resolve('Match Id is required');
 
-            const options = {
-                url: 'https://www.cricbuzz.com' + url,
-                headers: {
-                    'User-Agent': 'request'
-                }
-            };
+            url = 'https://www.cricbuzz.com' + url;
+            const response = await this.utilsObj.fetchData(url);
 
-            let tournamentName = await this.getTournamentName(options);
+            let tournamentName = await this.getTournamentName(response);
 
-            let response = await this.getMatchStatsByMatchId(options);
-            response['tournamentName'] = tournamentName;
+            let finalResponse = await this.getMatchStatsByMatchId(response);
+            finalResponse['tournamentName'] = tournamentName;
 
-            return Promise.resolve(response);
+            return Promise.resolve(finalResponse);
         } catch (error) {
             writeLogError(['matchStats | scrapeData |', error]);
             return Promise.reject(error);
         }
     }
 
-    private getTournamentName(options): Promise<{}> {
-        return new Promise(async (resolve, reject) => {
-            if (!this.matchId) return resolve('Match Id is required');
-
-            try {
-                const response = await axios(options);
-                if (response.status === 200) {
-                    const $ = cheerio.load(response.data);
-
-                    $('.cb-col.cb-col-100.cb-bg-white').each((i, el) => {
-                        const tournamentName = $(el).find('a').attr('title');
-                        return resolve(tournamentName);
-                    });
-                }
-            } catch (error) {
-                return reject(error);
+    private async getTournamentName($): Promise<string[]> {
+        try {
+            const elements = $('.cb-col.cb-col-100.cb-bg-white');
+            if (elements.length === 0) {
+                throw new Error('No elements found with the selector .cb-col.cb-col-100.cb-bg-white');
             }
-        });
+            const tournamentNames = elements.map((i, el) => $(el).find('a').attr('title')).get();
+            return tournamentNames[0];
+        } catch (error) {
+            throw new Error(`Error while fetching tournament name: ${error.message}`);
+        }
     }
 
-    private getMatchStatsByMatchId(options): Promise<{}> {
-        return new Promise(async (resolve, reject) => {
-            let matchData = {};
+    private getMatchStatsByMatchId($): Promise<{}> {
+        let matchData = {};
 
-            try {
-                const response = await axios(options);
-                if (response.status === 200) {
-                    const $ = cheerio.load(response.data);
+        try {
+            let currentTeamScoreString = $('span.cb-font-20.text-bold').text().trim();
+            let otherTeamScoreString = $('div.cb-text-gray.cb-font-16').text().trim();
+            const currentTeamDataArray = this.getTeamData(currentTeamScoreString);
+            const otherTeamDataArray = this.getTeamData(otherTeamScoreString);
 
-                    let currentTeamScoreString = $('span.cb-font-20.text-bold').text().trim();
-                    let otherTeamScoreString = $('div.cb-text-gray.cb-font-16').text().trim();
-                    const currentTeamDataArray = currentTeamScoreString.includes('&') ? this.parseTeamDataForTestMatches(currentTeamScoreString) : currentTeamScoreString.split(/[/\s/\-/\(/\)]/).filter(Boolean);
-                    const otherTeamDataArray = otherTeamScoreString.includes('&') ? this.parseTeamDataForTestMatches(otherTeamScoreString) : otherTeamScoreString.split(/[/\s/\-/\(/\)]/).filter(Boolean);
-                    const currentBatsman = $('div.cb-col.cb-col-50').eq(1).find('a').text();
-                    const currentBatsmanRuns = $('div.cb-col.cb-col-10.ab.text-right').eq(0).text();
-                    const currentBatsmanBalls = $('div.cb-col.cb-col-10.ab.text-right').eq(1).text();
-                    const otherBatsman = $('div.cb-col.cb-col-50').eq(2).find('a').text();
-                    const otherBatsmanRuns = $('div.cb-col.cb-col-10.ab.text-right').eq(2).text();
-                    const otherBatsmanBalls = $('div.cb-col.cb-col-10.ab.text-right').eq(3).text();
+            matchData = {
+                matchId: this.matchId,
+                team1: this.getTeamObject(currentTeamDataArray, true),
+                team2: this.getTeamObject(otherTeamDataArray, false),
+                onBatting: {
+                    player1: this.getBatsmanData($, 0),
+                    player2: this.getBatsmanData($, 1)
+                },
+                summary: $('div.cb-text-stumps, div.cb-text-complete, div.cb-text-inprogress').text().trim()
+            };
 
-                    matchData = {
-                        matchId: this.matchId,
-                        team1: !currentTeamDataArray[0] ? {} : {
-                            isBatting: true,
-                            name: currentTeamDataArray[0],
-                            score: currentTeamDataArray[1],
-                            overs: currentTeamDataArray.length > 3 ? currentTeamDataArray[3] : currentTeamDataArray[2],
-                            wickets: currentTeamDataArray.length > 3 ? currentTeamDataArray[2] : "10",
-                        },
-                        team2: !otherTeamDataArray[0] ? {} : {
-                            isBatting: false,
-                            name: otherTeamDataArray[0],
-                            score: otherTeamDataArray[1],
-                            overs: otherTeamDataArray.length > 3 ? otherTeamDataArray[3] : otherTeamDataArray[2],
-                            wickets: otherTeamDataArray.length > 3 ? otherTeamDataArray[2] : "10",
-                        },
-                        onBatting: {
-                            player1: {
-                                name: currentBatsman,
-                                onStrike: true,
-                                runs: currentBatsmanRuns,
-                                balls: currentBatsmanBalls
-                            },
-                            player2: {
-                                name: otherBatsman,
-                                onStrike: false,
-                                runs: otherBatsmanRuns,
-                                balls: otherBatsmanBalls
-                            }
-                        },
-                        summary: $('div.cb-text-stumps, div.cb-text-complete, div.cb-text-inprogress').text().trim()
-                    };
+            this.addPreviousInningsData(matchData, currentTeamDataArray, 'team1');
+            this.addPreviousInningsData(matchData, otherTeamDataArray, 'team2');
 
-                    // add previousInnings data if present in team1 and team2
-                    if (currentTeamDataArray.length > 4) {
-                        matchData['team1']['previousInnings'] = this.appendPreviousInningsData(currentTeamDataArray[4]);
-                    }
+            return Promise.resolve(matchData);
+        } catch (error) {
+            writeLogError(['matchStats | getMatchStatsByMatchId |', error]);
+            throw error;
+        }
+    }
 
-                    if (otherTeamDataArray.length > 4) {
-                        matchData['team2']['previousInnings'] = this.appendPreviousInningsData(otherTeamDataArray[4]);
-                    }
+    private getTeamData(scoreString: string): string[] {
+        return scoreString.includes('&') ? this.parseTeamDataForTestMatches(scoreString) : scoreString.split(/[/\s/\-/\(/\)]/).filter(Boolean);
+    }
 
-                    return resolve(matchData);
-                }
-            } catch (error) {
-                return reject(error);
-            }
-        });
+    private getTeamObject(teamDataArray: string[], isBatting: boolean): {} {
+        return !teamDataArray[0] ? {} : {
+            isBatting: isBatting,
+            name: teamDataArray[0],
+            score: teamDataArray[1],
+            overs: teamDataArray.length > 3 ? teamDataArray[3] : teamDataArray[2],
+            wickets: teamDataArray.length > 3 ? teamDataArray[2] : "10",
+        };
+    }
+
+    private getBatsmanData($, index: number): { name: string, runs: string, balls: string } {
+        return {
+            name: $('div.cb-col.cb-col-50').eq(index + 1).find('a').text(),
+            runs: $('div.cb-col.cb-col-10.ab.text-right').eq(index * 2).text(),
+            balls: $('div.cb-col.cb-col-10.ab.text-right').eq(index * 2 + 1).text()
+        };
+    }
+
+    private addPreviousInningsData(matchData: {}, teamDataArray: string[], team: string) {
+        if (teamDataArray.length > 4) {
+            matchData[team]['previousInnings'] = {
+                runs: teamDataArray[0],
+                wickets: teamDataArray.length > 1 ? teamDataArray[1] : "10"
+            };
+        }
     }
 
     private parseTeamDataForTestMatches(scoreString): Array<any> {
         // Regular expression pattern to capture team name, score, wickets, and overs
         const pattern = /([A-Za-z\s]+)\s+(\d+(?:\/\d+)?)(?:\s+&\s+(\d+\/\d+\s+\(\d+(?:\.\d+)?\)))?/;
-        let teamName = "", firstInnings = "", secondInnings = "", firstInningsData = [], secondInningsData = [];
+        let teamName = "", firstInnings = "", secondInnings = "", firstInningsData: string[] = [], secondInningsData: string[] = [];
 
         const matchData = scoreString.match(pattern);
 
@@ -229,13 +200,5 @@ export class MatchStats {
         }
 
         return secondInningsData.length > 0 ? [teamName, ...secondInningsData, firstInningsData] : [teamName, ...firstInningsData];
-    }
-
-    private appendPreviousInningsData(dataArray) {
-        // dataArray is an array containing [ runs, wickets ], flatten it to an object { runs, wickets }. If only runs is present, then wickets will be 10
-        return {
-            runs: dataArray[0],
-            wickets: dataArray.length > 1 ? dataArray[1] : "10"
-        };
     }
 }
