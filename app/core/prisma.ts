9f71927d45b9f7afc85a532d1d6a5fc746c93a57
deveshsangwan <dev.sangwan2001@gmail.com>
2025-02-24 00:55:21 +0530

    testing

--- app/core/prisma.ts
+++ app/core/prisma.ts
@@ -1,15 +1,17 @@
 import { PrismaClient } from '@prisma/client';
-import { withOptimize } from '@prisma/extension-optimize';
+// import { withOptimize } from '@prisma/extension-optimize';
 
-const extendedPrisma = () => new PrismaClient().$extends(withOptimize({ apiKey: process.env.OPTIMIZE_API_KEY }));
-type PrismaClientExtended = ReturnType<typeof extendedPrisma>;
+// const extendedPrisma = () => new PrismaClient().$extends(withOptimize({ apiKey: process.env.OPTIMIZE_API_KEY }));
+// type PrismaClientExtended = ReturnType<typeof extendedPrisma>;
 
 declare global {
     // eslint-disable-next-line no-unused-vars
-    var prisma: PrismaClientExtended | undefined;
+    // var prisma: PrismaClientExtended | undefined;
+    var prisma: PrismaClient | undefined;
 }
 
-const prisma = global.prisma || extendedPrisma();
+// const prisma = global.prisma || extendedPrisma();
+const prisma = global.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') { global.prisma = prisma; }
 export default prisma;
--- app/dist/core/prisma.js
+++ app/dist/core/prisma.js
@@ -1,9 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const client_1 = require("@prisma/client");
-const extension_optimize_1 = require("@prisma/extension-optimize");
-const extendedPrisma = () => new client_1.PrismaClient().$extends((0, extension_optimize_1.withOptimize)({ apiKey: process.env.OPTIMIZE_API_KEY }));
-const prisma = global.prisma || extendedPrisma();
+// const prisma = global.prisma || extendedPrisma();
+const prisma = global.prisma || new client_1.PrismaClient();
 if (process.env.NODE_ENV !== 'production') {
     global.prisma = prisma;
 }
